38ef422d55eca3bd46872f7a061da17efde33909
awp-sirius <awpsirius@gmail.com>
2025-10-26 13:26:56 +0300

    create

---
+++ MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MauiCollectionViewError;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+        ColView.ItemsSource = new List<string>()
+        {
+            "1","2","3","4"
+        };
+    }
+}
